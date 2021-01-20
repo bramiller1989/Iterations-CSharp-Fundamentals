075a2dc0be983f8aa550676895c821e771773b20
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 13:40:32 -0600

    add a range

--- Program.cs
+++ Program.cs
@@ -81,7 +81,7 @@ namespace Iterations
             Console.WriteLine("\nEcho random numbers in a for loop");
             var random = new Random();
             for (var i = 0; i < 10; i++)
-                Console.WriteLine(random.Next());
+                Console.WriteLine(random.Next(1, 10));
         }
     }
 }
