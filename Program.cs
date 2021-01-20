4813e3ab4fbe0973c8e7c024342448dff7ecec78
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 13:20:47 -0600

    use for loop and for each loop

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Count up by 2 using for loop");
+
             for (var i = 1; i <= 10; i++)
             {
                 if (i % 2 == 0)
@@ -14,12 +16,33 @@ namespace Iterations
                 }
             }
 
+            Console.WriteLine("\n");
+            Console.WriteLine("Count down by 2 using for loop");
+
             for (var i = 10; i >= 1; i--)
             {
                 if (i % 2 == 0)
                 {
                     Console.WriteLine(i);
                 }
+            }
+            
+            Console.WriteLine("\n");
+            Console.WriteLine("Iterate over string using for loop");
+
+            var name = "John Smith";
+
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Iterate over string using for each loop");
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
             }
         }
     }
