98d99eff75c481cfcbef26a7dd11bcfea92ab0c4
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 13:11:02 -0600

    add for loops

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (var i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
